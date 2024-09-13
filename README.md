a024fd60df3215f571dd8212275c6c0136bbcb70
joncepeda <jonathan.cepeda@inscoder.com>
2024-09-13 09:22:18 +0800

    added cat

---
+++ README.md
@@ -0,0 +1,3 @@
+<picture>
+  <img alt="Jon cepeda portfolio icon" src="https://user-images.githubusercontent.com/25423296/163456779-a8556205-d0a5-45e2-ac17-42d089e3c3f8.png">
+</picture>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ["./src/**/*.{js,cjs,mjs,ts,html}", "./*.html"],
   theme: {
     extend: {},
   },
